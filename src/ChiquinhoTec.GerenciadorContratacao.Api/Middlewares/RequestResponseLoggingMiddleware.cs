3ab0a32a73839cc88dfe28798235fb2e5c8ac334
Jackson Veroneze <jackson@jacksonveroneze.com>
2020-01-19 22:55:34 -0300

    Adjustment.

--- src/ChiquinhoTec.GerenciadorContratacao.Api/Middlewares/RequestResponseLoggingMiddleware.cs
+++ src/ChiquinhoTec.GerenciadorContratacao.Api/Middlewares/RequestResponseLoggingMiddleware.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Microsoft.IO;
+using System.IAsyncDisposable;
 
 namespace ChiquinhoTec.GerenciadorContratacao.Api.Middlewares
 {
